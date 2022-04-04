47683930beab07c7275de359e62c6091b89dd1e6
bcaudillo <btcaudillo@gmail.com>
2022-04-04 10:32:20 -0600

    commit tree 4aaf48981ccc77c38c35eb787079b90fc05f3913

--- index.js
+++ index.js
@@ -10,7 +10,27 @@ const tutorials = [
   'what is the difference between event capturing and bubbling?',
   'what is JSONP?'
 ];
-
+//map has an array and a condition. It will pass that array and print to the new array if the condition is passed. 
 const titleCased = () => {
-  return tutorials
+  return tutorials.map(string =>{
+    let arrayOfWords = string.split(' ');
+    const capatilizeWords = arrayOfWords.map(word=>{
+      let x=word[0].toUpperCase()
+      let y=word.slice(1)
+      return x+y;
+    })
+    return capatilizeWords.join(' ')
+        // console.log(capatilizeWords)
+  })
 }
+titleCased()
+  // for (const letter of array){
+  //   letter[0]=letter[0].toUpperCase();
+  //   //how do I parse through each letter? Another loop?
+  //   if(array[letter]===''){
+  //       return array[letter+1].toUpperCase();
+  //     }
+  // }
+
+//   const words = str.split(' ');
+// console.log(words[3]);
